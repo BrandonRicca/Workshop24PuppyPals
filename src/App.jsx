d9f60fbc4c6cb6899075dcd68e9608f1d7695a2a
BrandonRicca <tulshibrandon@gmail.com>
2023-11-06 08:55:28 -0500

    feature puppy and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import './App.css'
+import './index.css'
 import { puppyList } from './data.js'
 import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
   console.log("puppyList: ", puppyList)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  {
+    featPupId && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    )
+  }
   return (
     <>
       <div className="App">
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
-        }
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={() => { }} key={puppy.id}>
+              {puppy.name}
+              {() => { console.log("puppy id: ", puppy.id) }}
+              onClick={() => { setFeatPupId(puppy.id) }}
+            </p>
+          );
+        })}
       </div>
     </>
   )
